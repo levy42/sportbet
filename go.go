375b768cedef4bc83a8d701fb723eb548a6d10c4
Grigoriy Rozhkov <groghkov@maprtech.com>
2016-10-31 19:59:17 +0200

    commit

--- go.go
+++ go.go
@@ -4,39 +4,70 @@ import (
 	_"fmt"
 	"encoding/json"
 	"net/http"
+	"github.com/jinzhu/gorm"
+	_"github.com/mattn/go-sqlite3"
 )
 
 type MarketType struct {
-	Id         int
-	Name       string
-	Code       string
-	Outcomes   []string
-	calculator func(source map[int]Market) []Market
-	sources    []MarketType
+	Id         int `json:"id"`
+	Name       string `json:"name"`
+	Code       string `json:"code"`
+	Outcomes   []string `json:"outcomes" gorm:"-"`
+	SportId string `json:"sport_id"`
+	calculator func(source map[int]Market) []Market `gorm:"-"`
+	sources    []MarketType `gorm:"-"`
 }
 
 type Market struct {
-	Id        int
-	Name      string
-	Code      string
-	Outcomes  []float32
-	Parameter float32
+	Id        int `json:"id"`
+	Name      string `json:"n"`
+	Code      string `json:"c"`
+	Outcomes  []float32 `json:"o" gorm:"-"`
+	Parameter float32 `json:"p" gorm:"-"`
+}
+
+type Outcome struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Value float32 `json:"value"`
+	Parameter float32 `json:"parameter"`
+	Code string `json:"code"`
+	FullCode string `json:"full_code"`
+	MarketId float32 `json:"market_id"`
+}
+
+type TestModel struct {
+	gorm.Model
+	name string
 }
 
 func marketTypes() []MarketType {
 	return []MarketType{
-		{1, "NoDraw", "NO_DRAW", []string{"1", "2"}, nil, nil},
+		{Id:1, Name:"NoDraw",Code: "NO_DRAW",Outcomes: []string{"1", "2"}},
 	}
+}
+
+func calculate(sources []Market) {
+
 }
 
 func Hello(rw http.ResponseWriter, request *http.Request) {
 	rw.Write([]byte("Hello world."))
 }
-func MarketTypes(rw http.ResponseWriter, request *http.Request)  {
-	r, _ :=json.Marshal(marketTypes())
+func MarketTypes(rw http.ResponseWriter, request *http.Request) {
+	r, _ := json.Marshal(marketTypes())
 	rw.Write(r)
 }
 func main() {
+	db, err := gorm.Open("sqlite3", "app.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+	db.AutoMigrate(&MarketType{})
+	db.AutoMigrate(&MarketType{})
+	db.AutoMigrate(&Outcome{})
+	db.AutoMigrate(&TestModel{})
 	http.HandleFunc("/", Hello)
 	http.HandleFunc("/markets", MarketTypes)
 	http.ListenAndServe(":3000", nil)
