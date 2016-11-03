1fc42db1c7f117612ac92e64de7c79d47e65da01
Grigoriy Rozhkov <groghkov@maprtech.com>
2016-11-03 20:41:50 +0200

    comm

--- go.go
+++ go.go
@@ -3,11 +3,12 @@ package main
 import (
 	"encoding/json"
 	"github.com/jinzhu/gorm"
-	//_ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 	"io/ioutil"
 	"net/http"
 	"runtime"
 	"strconv"
+	"log"
 )
 
 type Sport struct {
@@ -15,14 +16,12 @@ type Sport struct {
 	Name string `json:"name"`
 }
 type OutcomeType struct {
-	Id           int     `json:"id"`
-	Name         string  `json:"name"`
-	Value        float32 `json:"value"`
-	Parameter    float32 `json:"parameter"`
-	Code         string  `json:"code"`
-	FullCode     string  `json:"full_code"`
-	MarketTypeId int     `json:"market_type_id"`
-	SportId      int     `json:"sport_id"`
+	Id        int     `json:"id"`
+	Name      string  `json:"name"`
+	Parameter float32 `json:"parameter"`
+	Code      string  `json:"code"`
+	FullCode  string  `json:"full_code"`
+	SportId   int     `json:"sport_id"`
 }
 type Outcome struct {
 	Name          string  `json:"name"`
@@ -35,8 +34,8 @@ type Model struct {
 	SportId      int                  `json:"sport_id"`
 	Description  string               `json:"description"`
 	InputParams  []Parameter          `json:"input_params"`
-	TablesStage1 map[int]OutcomeTable `json:"tables_stage_1,omitempty"`
-	TablesStage2 map[int]OutcomeTable `json:"tables_stage_2,omitempty"`
+	TablesStage1 *map[int]*OutcomeTable `json:"tables_stage_1,omitempty"`
+	TablesStage2 *map[int]*OutcomeTable `json:"tables_stage_2,omitempty"`
 }
 type OutcomeTable struct {
 	ParamsIds   []int       `json:"key_param"` // param that effect on the outcome
@@ -48,19 +47,14 @@ type Parameter struct {
 	Id          int             `json:"id"`
 	Name        string          `json:"name"`
 	Description string          `json:"description"`
+	Type        int             `json:"type"`
 	ValueMap    map[float32]int `json:"value_map,omitempty"` // int represent of float value of outcome, example {1.01:1, 1.02:2 ...}
 	VRange      int             `json:"v_range,omitempty"`
 }
 
-func loadModel(name string) {
-	b, _ := ioutil.ReadFile("models/" + name) // just pass the file name
-	var model Model
-	json.Unmarshal(b, &model) // very large operation
-	models[model.Id] = model
-}
-
 var db *gorm.DB
 var models map[int]Model
+var modelsMeta map[int]Model
 
 func main() {
 	runtime.GOMAXPROCS(4) // 4 cores, input your number of cores
@@ -86,32 +80,35 @@ func Sports(rw http.ResponseWriter, request *http.Request) {
 }
 
 func OutcomeTypes(rw http.ResponseWriter, request *http.Request) {
-	sportIds := request.URL.Query()["sport"]
 	var outcomes []OutcomeType
-	db.Where("sport_id in (?)", sportIds).Find(&outcomes)
+	db.Find(&outcomes)
 	result, _ := json.Marshal(outcomes)
 	rw.Write(result)
 }
 
 func Models(rw http.ResponseWriter, request *http.Request) {
-	result, _ := json.Marshal(models)
+	result, _ := json.Marshal(modelsMeta)
 	rw.Write(result)
 }
 
 func Calculate(rw http.ResponseWriter, request *http.Request) {
-	type HashedParam struct{ value, vRange int }
 	modelId, _ := strconv.Atoi(request.URL.Query()["model"][0])
-	params := request.URL.Query()["model"]
-	model := models[modelId]
+	params, _ := request.URL.Query()["params"]
+	model, ok := models[modelId]
+	if !ok {
+		rw.WriteHeader(201)
+		return
+	}
+	type HashedParam struct{ value, vRange int }
 	var hashedParams map[int]HashedParam
 	for i, v := range params {
 		option := model.InputParams[i]
 		float_64value, _ := strconv.ParseFloat(v, 32)
 		hashedParams[i] = HashedParam{option.ValueMap[float32(float_64value)], option.VRange}
 	}
-	outcomes := make([]Outcome, len(model.TablesStage1) + len(model.TablesStage2))
+	outcomes := make([]Outcome, len(*model.TablesStage1) + len(*model.TablesStage2))
 	var outcome_index int
-	getValue := func(t OutcomeTable) float32 {
+	getValue := func(t *OutcomeTable) float32 {
 		var hash int
 		if len(t.ParamsIds) == 1 {
 			hash = hashedParams[t.ParamsIds[0]].value
@@ -123,7 +120,7 @@ func Calculate(rw http.ResponseWriter, request *http.Request) {
 		}
 		return t.Values[hash]
 	}
-	for _, t := range model.TablesStage1 {
+	for _, t := range *model.TablesStage1 {
 		value := getValue(t)
 		if value == 0 {
 			continue
@@ -134,7 +131,7 @@ func Calculate(rw http.ResponseWriter, request *http.Request) {
 		}
 		outcome_index++
 	}
-	for _, t := range model.TablesStage1 {
+	for _, t := range *model.TablesStage1 {
 		value := getValue(t)
 		if value == 0 {
 			continue
@@ -149,6 +146,25 @@ func Calculate(rw http.ResponseWriter, request *http.Request) {
 func LoadModel(rw http.ResponseWriter, request *http.Request) {
 	name := request.URL.Query()["name"][0]
 	go loadModel(name)
+}
+
+func loadModel(name string) {
+	b, err := ioutil.ReadFile("models/" + name) // just pass the file name
+	if err != nil {
+		log.Fatalf("Failed to load new model, no such file %s", name)
+	}
+	var model Model
+	json.Unmarshal(b, &model) // very large operation
+	models[model.Id] = model
+	paramsMeta := make([]Parameter, len(model.InputParams))
+	for i := range paramsMeta {
+		paramsMeta[i].Id = model.InputParams[i].Id
+		paramsMeta[i].Description = model.InputParams[i].Description
+		paramsMeta[i].Name = model.InputParams[i].Name
+	}
+	modelMeta := Model{Id:model.Id, Description:model.Description, SportId:model.SportId, InputParams:paramsMeta}
+	modelsMeta[model.Id] = modelMeta
+	log.Printf("New model loaded, Id = %d", model.Id)
 }
 
 func hashForTwo(p1, p2, range1, range2 int) int {
